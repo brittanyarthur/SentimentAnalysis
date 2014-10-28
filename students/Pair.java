34074e0ca10acaaca2f3cee7e3278e8ef0e35f4a
Brittany Arthur <brittany_arthur@hotmail.com>
2014-10-28 00:15:35 -0700

    working build! returns only top score

--- students/Pair.java
+++ students/Pair.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 
 class Pair{
 
@@ -13,10 +14,16 @@ class Pair{
   	 	  }
      }
 
+    public static class Max {
+    	public static int score;
+    	public static String students;
+    	public static Stack<String> st;
+	}
+
 	public static void main(String[] args){
 		//set table
 		Hashtable<String, Integer> pair_table = new Hashtable<String, Integer>();
-		pair_table.put("AB",2);
+		pair_table.put("AB",100);
 		pair_table.put("AC",1);
 		pair_table.put("AD",1);
 		pair_table.put("AE",1);
@@ -35,43 +42,45 @@ class Pair{
 		pair_table.put("DF",1);
 
 		pair_table.put("EF",2);
-		bestMatch match = new bestMatch("",0);
-		Score("ABCDEF", pair_table, match);
-		System.out.println("Max score is: " + match.score + "\nBest Group: " + match.student_set);
+
+		Max.st = new Stack<String>();
+		Max.students = "";
+		int scoretotal = Score("ABCDEF", pair_table);
+
+		System.out.println("Max score is: " + scoretotal + "\n");
+		//System.out.println("\n\nStudent set is: "+match.student_set);
 	}
 
-	public static int Score(String remainder, Hashtable<String, Integer> table, bestMatch match){
+	public static int Score(String remainder, Hashtable<String, Integer> table){
 		if(remainder.length() == 0){
 			return 0;
 		}
-		int max = 0;
 		int score = 0;
-		int tempmax = 0;
+		int max = 0;
 		int current_pair_score = 0;
 		int candidate_score = 0;
 		for(int index=1; index < remainder.length(); index++){
+
 			if(table.get(remainder.charAt(0)+remainder.charAt(index)) == null){
 				//insert into the hashtable
 			}
-			StringBuilder remainder_copy = new StringBuilder(remainder);
-			remainder_copy.deleteCharAt(index); 
-			remainder_copy.deleteCharAt(0);
+			//get pair score
 			String lookup_pair = remainder.charAt(0)+""+remainder.charAt(index)+"";
-			System.out.println("pair is: " + lookup_pair);
-			current_pair_score = table.get(remainder.charAt(0)+""+remainder.charAt(index));
-		    candidate_score = current_pair_score + Score(remainder_copy.length()==0?"":remainder_copy.toString(), table, match);
-		    System.out.println("Current max is: " + candidate_score);
-			if(candidate_score > match.score){
-				
-				//System.out.println("pair is: " + lookup_pair +"\n");
-				max = candidate_score;
-				match.score = candidate_score;
-				///match.student_set += lookup_pair + temp_match.student_set;
-				//match.score = candidate_score;
-				//max = candidate_score;
+			current_pair_score = table.get(remainder.charAt(0)+""+remainder.charAt(index)+"");
+
+			//create the substring to pass in to look for smaller substrings
+			StringBuilder remainder_copy = new StringBuilder(remainder);
+			remainder_copy.deleteCharAt(0); 
+			remainder_copy.deleteCharAt(index-1);
+			candidate_score = Score(remainder_copy.toString(), table);
+
+			//gets max at this level of the recursion
+			if(max < (current_pair_score+candidate_score)){
+			   max = current_pair_score+candidate_score;
+			   System.out.println(lookup_pair + " with max = " + max );
 			}
 		}
-		return candidate_score;
+		return max;
 	}
 }
 
